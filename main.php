1dd41bb65477fdc17e7947232f2f7ce041c23ae4
町田 直基 <n-machida@ceres-inc.jp>
2019-07-26 13:54:37 +0900

    クラス分けやめる。

--- main.php
+++ main.php
@@ -1,8 +1,36 @@
 <?php
 require_once "vendor/autoload.php";
 
-use lib\ErrorTail;
+$sleepInterval = 1;
+$filePath = "./test.log";
 
-$errorTail = new ErrorTail("./test.log");
+$fp = fopen($filePath, 'r');
 
-$errorTail->run();
+// 一番末尾のポインタの位置を取得
+$position = $this->getLastPosition($fp);
+
+do{
+
+    // 一番末尾のポインタの位置を取得
+    $nextPosition = $this->getLastPosition($fp);
+
+    // ポインタが増えている場合
+    if($nextPosition > $position){
+
+        // 現在のポインタをセット
+        fseek($fp, $position);
+
+        // 現在のポインタから増加分のデータを取得
+        $record = fread($fp, $nextPosition - $position);
+
+        if(preg_match("/^ERROR/", $record)) {
+            echo $record;
+        }
+    }
+
+    // ポインタの更新
+    $position = $nextPosition;
+
+    sleep($sleepInterval);
+
+}while(true);
